79d135658ad7112d1a868c0e3e3723be2fdd9835
Jesus Castilla <jisusquiros2001@gmail.com>
2021-06-04 05:22:58 -0600

    Menu y aerca de
    se termino el menu ne navegacion y la pagina acerca de

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                     "~/Scripts/src/Main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/MyStyle.css",
+                      "~/Content/about.css"));
         }
     }
 }
