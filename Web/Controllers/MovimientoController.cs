6e7963b4df473326c67f81a30cb9e8122feeedb9
Jesus Castilla <jisusquiros2001@gmail.com>
2021-07-17 22:49:09 -0600

    99 terminado

--- Web/Controllers/MovimientoController.cs
+++ Web/Controllers/MovimientoController.cs
@@ -41,7 +41,7 @@ namespace Web.Controllers
             {
                 ou = new Salida();
                 Session["out"] = ou;
-                TempData["detalle"]= ou.SalidaProducto;
+                TempData["detalle"]= ou.SalidaProducto.ToList();
                
 
             }
@@ -49,7 +49,89 @@ namespace Web.Controllers
             return PartialView(ou);
         }
         
+        public PartialViewResult saveSalidaProducto(int cantidad,int Seccion, long producto)
+        {
+            List<SalidaProducto> salidas = (List<SalidaProducto>)TempData["detalle"];
+            IserviceProducto iservice = new ServiceProducto();
+            IServiceSeccion seccion = new ServiceSeccion();
+            SalidaProducto salidaProducto = salidas.FirstOrDefault(x => x.IdProducto == producto);
+            if (salidaProducto == null)
+            {
+              
+                 salidaProducto = new SalidaProducto();
+                salidaProducto.Cantidad = cantidad;
+                salidaProducto.IdSeccion = Seccion;
+                salidaProducto.Seccion = seccion.GetSeccion().FirstOrDefault(x => x.Id == Seccion);
+                salidaProducto.IdProducto = producto;
+                salidaProducto.Producto = iservice.GetProductoById(producto);
 
-       
+                salidas.Add(salidaProducto);
+            }
+            else
+            {
+                if (salidaProducto.IdSeccion != Seccion)
+                {
+                    salidaProducto = new SalidaProducto();
+                    salidaProducto.Cantidad = cantidad;
+                    salidaProducto.IdSeccion = Seccion;
+                    salidaProducto.Seccion = seccion.GetSeccion().FirstOrDefault(x => x.Id == Seccion);
+                    salidaProducto.IdProducto = producto;
+                    salidaProducto.Producto = iservice.GetProductoById(producto);
+
+                    salidas.Add(salidaProducto);
+                }
+                else
+                {
+                    int c = salidas.IndexOf(salidaProducto);
+                  salidaProducto = new SalidaProducto();
+                    salidaProducto.Cantidad = cantidad;
+                    salidaProducto.IdSeccion = Seccion;
+                    salidaProducto.Seccion = seccion.GetSeccion().FirstOrDefault(x => x.Id == Seccion);
+                    salidaProducto.IdProducto = producto;
+                    salidaProducto.Producto = iservice.GetProductoById(producto);
+                    salidas[c] = salidaProducto;
+                }
+              
+            }
+
+            return PartialView("_producto");
+        }
+        public PartialViewResult eliminarpro(int index)
+        {
+            List<SalidaProducto> salidas = (List<SalidaProducto>)TempData["detalle"];
+            salidas.RemoveAt(index);
+            TempData["detalle"] = salidas;
+            return PartialView("_producto");
+        }
+        public PartialViewResult listarNombre(string filtro)
+        {
+            IEnumerable<Producto> lista = null;
+            ServiceProducto service = new ServiceProducto();
+
+
+            try
+            {
+                if (String.IsNullOrEmpty(filtro))
+                {
+                    lista = service.GetProducto();
+                }
+                else
+                {
+                    lista = service.GetProductoBy(filtro);
+                }
+                TempData["productos"] =lista;
+                return PartialView("_listaproducto", lista);
+
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, System.Reflection.MethodBase.GetCurrentMethod());
+            }
+            TempData["productos"] = lista;
+            return PartialView("_listaproducto", lista);
+        }
+
+
+
     }
 }
